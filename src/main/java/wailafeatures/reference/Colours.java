1793c39c22ce882e01d856ee9f875f7ea7b42380
way2muchnoise <jens.baetens@live.be>
2015-05-15 21:41:33 -0200

    delete unused statics in Colours

--- src/main/java/wailafeatures/reference/Colours.java
+++ src/main/java/wailafeatures/reference/Colours.java
@@ -2,19 +2,6 @@ package wailafeatures.reference;
 
 public class Colours
 {
-    public static final int BLACK = -16777216;
-    public static final int BLUE = -16776961;
-    public static final int CYAN = -16711681;
-    public static final int DRKGRAY = -12303292;
-    public static final int GRAY = -7829368;
-    public static final int GREEN = -16711936;
-    public static final int LTGRAY = -3355444;
-    public static final int MAGENTA = -65281;
-    public static final int RED = -65536;
-    public static final int TRANSPARENT = 0;
-    public static final int WHITE = -1;
-    public static final int YELLOW = -256;
-
     public static float getRed(int color)
     {
         return (color >> 16 & 255) / 255.0F;
