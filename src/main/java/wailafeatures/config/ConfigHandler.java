0ce07dd8189447400aaf20c4362d27571d974dbd
way2muchnoise <jens.baetens@live.be>
2016-04-12 21:34:44 +0200

    bump versions and remove old settings of removed features

--- src/main/java/wailafeatures/config/ConfigHandler.java
+++ src/main/java/wailafeatures/config/ConfigHandler.java
@@ -35,12 +35,7 @@ public class ConfigHandler
     private static void loadConfig()
     {
         Settings.debugMode = config.getBoolean(TranslationHelper.translateToLocal("wailafeatures.config.debug.title"), Configuration.CATEGORY_GENERAL, false, TranslationHelper.translateToLocal("wailafeatures.config.debug.description"));
-        Settings.fuzzyColourMode = config.getBoolean(TranslationHelper.translateToLocal("wailafeatures.config.fuzzyColour.title"), Configuration.CATEGORY_GENERAL, true, TranslationHelper.translateToLocal("wailafeatures.config.fuzzyColour.description"));
-        Settings.colourPrefix = config.getBoolean(TranslationHelper.translateToLocal("wailafeatures.config.colourPrefix.title"), Configuration.CATEGORY_GENERAL, true, TranslationHelper.translateToLocal("wailafeatures.config.colourPrefix.description"));
-        Settings.materialPrefix = config.getBoolean(TranslationHelper.translateToLocal("wailafeatures.config.materialPrefix.title"), Configuration.CATEGORY_GENERAL, true, TranslationHelper.translateToLocal("wailafeatures.config.materialPrefix.description"));
         Settings.authorIdent = config.getBoolean(TranslationHelper.translateToLocal("wailafeatures.config.authorIdent.title"), Configuration.CATEGORY_GENERAL, true, TranslationHelper.translateToLocal("wailafeatures.config.authorIdent.description"));
-        Settings.colourSearch = config.getBoolean(TranslationHelper.translateToLocal("wailafeatures.config.colourSearch.title"), Configuration.CATEGORY_GENERAL, true, TranslationHelper.translateToLocal("wailafeatures.config.colourSearch.description"));
-        Settings.materialSearch = config.getBoolean(TranslationHelper.translateToLocal("wailafeatures.config.materialSearch.title"), Configuration.CATEGORY_GENERAL, true, TranslationHelper.translateToLocal("wailafeatures.config.materialSearch.description"));
         if (config.hasChanged())
             config.save();
     }
--- src/main/java/wailafeatures/config/Settings.java
+++ src/main/java/wailafeatures/config/Settings.java
@@ -3,11 +3,5 @@ package wailafeatures.config;
 public final class Settings
 {
     public static boolean debugMode;
-    public static boolean fuzzyColourMode;
-    public static boolean colourPrefix;
-    public static boolean materialPrefix;
-
-    public static boolean colourSearch;
-    public static boolean materialSearch;
     public static boolean authorIdent;
 }
